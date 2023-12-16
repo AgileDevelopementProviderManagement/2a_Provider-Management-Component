240f0df5b95f502fa738abe22456b544b9cee9ee
pparaska <poonamparaskar2012@gmail.com>
2023-12-16 11:54:38 +0100

    Added jpa to pom.xml

---
+++ agile_development/src/main/java/com/frauas/agile_development/model/MasterAgreementType.java
@@ -0,0 +1,12 @@
+package com.frauas.agile_development.model;
+
+public class MasterAgreementType {
+
+	public MasterAgreementType() {
+		// TODO Auto-generated constructor stub
+	}
+//„valid from“, „valid until“, „dailyrate indicator“, „deadline“, „teamdeadline“, „workscontractdeadline
+	//Domains - ENUM values 
+	// List<Domains> domains - name, id 
+	// List<Subdomain>
+}
---
+++ agile_development/src/main/java/com/frauas/agile_development/model/Provider.java
@@ -0,0 +1,9 @@
+package com.frauas.agile_development.model;
+
+public class Provider {
+
+	public Provider() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
