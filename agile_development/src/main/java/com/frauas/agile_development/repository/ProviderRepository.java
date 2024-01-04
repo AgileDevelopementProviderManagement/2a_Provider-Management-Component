0d7160144dc2e456f65a209c244dd919e883a44d
Aneeta Antony <aneetakalathoor@gmail.com>
2024-01-04 11:01:24 +0100

    application properties mysql configurations

--- agile_development/src/main/java/com/frauas/agile_development/repository/ProviderRepository.java
+++ agile_development/src/main/java/com/frauas/agile_development/repository/ProviderRepository.java
@@ -3,8 +3,9 @@ package com.frauas.agile_development.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.frauas.agile_development.model.Provider;
+import org.springframework.stereotype.Repository;
 
-//@Repository
+@Repository
 public interface ProviderRepository extends JpaRepository<Provider, Integer> {
 
 
