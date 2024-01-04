05a2c3edc46fc3e52243bceeec9200e77015fc98
Aneeta Antony <aneetakalathoor@gmail.com>
2024-01-04 10:56:03 +0100

    Add Master agreement type REST APIs and service and repo class

--- agile_development/src/main/java/com/frauas/agile_development/repository/MasterAgreementTypeRepository.java
+++ agile_development/src/main/java/com/frauas/agile_development/repository/MasterAgreementTypeRepository.java
@@ -1,8 +1,9 @@
 package com.frauas.agile_development.repository;
 
-//@Repository
-public interface MasterAgreementTypeRepository {
+import com.frauas.agile_development.model.MasterAgreementType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
-
+@Repository
+public interface MasterAgreementTypeRepository extends JpaRepository<MasterAgreementType,Integer> {
 }
---
+++ agile_development/src/main/java/com/frauas/agile_development/service/MasterAgreementTypeServiceImpl.java
@@ -0,0 +1,54 @@
+package com.frauas.agile_development.service;
+
+import com.frauas.agile_development.model.MasterAgreementType;
+import com.frauas.agile_development.repository.MasterAgreementTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MasterAgreementTypeServiceImpl implements MasterAgreementTypeService {
+  @Autowired
+  private  MasterAgreementTypeRepository masterAgreementTypeRepository ;
+
+
+    @Override
+    public List<MasterAgreementType> getMasterAgreements() {
+        return masterAgreementTypeRepository.findAll();
+    }
+    @Override
+    public MasterAgreementType getMasterAgreement(int masterAgreementTypeId) {
+        return masterAgreementTypeRepository.findById(masterAgreementTypeId).orElseThrow(() ->
+                new MasterAgreeTypeNotFoundException("the requested master agreement type not found in db") );
+    }
+
+    @Override
+    public MasterAgreementType addMasterAgreement(MasterAgreementType masterAgreementType) {
+        return masterAgreementTypeRepository.save(masterAgreementType);
+    }
+
+    @Override
+    public MasterAgreementType updateMasterAgreement(int id, MasterAgreementType updatedMasterAgreement) throws MasterAgreeTypeNotFoundException{
+        Optional<MasterAgreementType> isInDb = masterAgreementTypeRepository.findById(id);
+
+        if (isInDb.isPresent()) {
+            MasterAgreementType newMAT = isInDb.get();
+
+            newMAT.setMasterAgreementTypeName(updatedMasterAgreement.getMasterAgreementTypeName());
+            newMAT.setValidFrom(updatedMasterAgreement.getValidFrom());
+            newMAT.setValidUntil(updatedMasterAgreement.getValidUntil());
+            return masterAgreementTypeRepository.save(newMAT);
+        }
+        else {
+            //return new MasterAgreeTypeNotFoundException();
+            return null;
+        }
+    }
+
+    @Override
+    public void deleteMasterAgreement(int id) {
+       masterAgreementTypeRepository.deleteById(id);
+    }
+}
