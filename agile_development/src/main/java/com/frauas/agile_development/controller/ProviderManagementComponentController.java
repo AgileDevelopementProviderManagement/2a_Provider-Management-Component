7858030ec1d8463a63f8ea3131ac9d167d3b8860
Sonam-22 <sonampriya7070@gmail.com>
2024-01-12 06:03:32 +0100

    Connects auth api backend

--- agile_development/src/main/java/com/frauas/agile_development/controller/ProviderManagementComponentController.java
+++ agile_development/src/main/java/com/frauas/agile_development/controller/ProviderManagementComponentController.java
@@ -1,14 +1,28 @@
 package com.frauas.agile_development.controller;
 
 import java.util.List;
+import java.util.Map;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.frauas.agile_development.model.*;
+import com.frauas.agile_development.service.AuthService;
 import com.frauas.agile_development.service.DomainService;
 import com.frauas.agile_development.service.MasterAgreementTypeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
 import com.frauas.agile_development.service.ProviderManagementComponentService;
 import com.frauas.agile_development.service.RoleService;
 
@@ -23,6 +37,8 @@ public class ProviderManagementComponentController {
     private DomainService domainService;
     @Autowired
     private RoleService roleService;
+    @Autowired
+    private AuthService authService;
 
     private StandardExperienceLevel experienceLevel;
 
@@ -35,6 +51,7 @@ public class ProviderManagementComponentController {
     public StandardTechCatalog[] getAllTechnologyCatalogs() {
         return technologiesCatalog.values();
     }
+
     @GetMapping("/experiencelevels")
     public StandardExperienceLevel[] getAllExpLevels() {
         return experienceLevel.values();
@@ -49,6 +66,7 @@ public class ProviderManagementComponentController {
     public List<StandardDomains> getAllDomains() {
         return domainService.getAllDomains();
     }
+
     @PostMapping("/adddomains")
     public List<StandardDomains> addDomains(@RequestBody List<StandardDomains> domains) {
         return domainService.addDomains(domains);
@@ -57,6 +75,11 @@ public class ProviderManagementComponentController {
     @PostMapping("/addProvider")
     public Provider addProvider(@RequestBody Provider provider) {
         return providerManagementComponentService.saveProvider(provider);
+    }
+
+    @PostMapping("/login")
+    public JsonNode login(@RequestBody Map<String, String> credentials) throws JsonProcessingException {
+        return authService.login(credentials);
     }
 
     @GetMapping("/providers")
